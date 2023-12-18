88b56e239bccd1fc861364942301f6e822debeb3
Zambo1998 <alessandrobrillante78@gmail.com>
2023-12-18 14:01:48 +0100

    PropertiesManagerV2

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+r
+Bsrc/main/java/it/alfasoft/propertiesmanager/PropertiesManager.java,2\f\2fcb7aaa64ec48e502c3b54a3cb8428949082aa5
+T
+$src/main/resources/config.properties,f\a\fac4e92d770a0a6287eaad0e53c4efa350987f47
---
+++ src/main/java/it/alfasoft/propertiesmanager/PropertiesManager.java
@@ -0,0 +1,38 @@
+package it.alfasoft.propertiesmanager;
+
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class PropertiesManager {
+    private static Properties prop;
+    public static Properties getProperties() throws FileNotFoundException{
+        //Singleton
+        if(prop==null){
+            prop = new Properties();
+            
+            String urlKey = "db.url";
+            String userKey = "db.user";
+            String passwordKey = "db.password";
+
+            String dbUrl = System.getenv(urlKey);
+            String dbUser = System.getenv(userKey);
+            String dbPassword = System.getenv(passwordKey);
+
+            if(dbUrl != null && dbUser != null && dbPassword != null){
+                prop.setProperty(urlKey, dbUrl);
+                prop.setProperty(userKey, dbUser);
+                prop.setProperty(passwordKey, dbPassword);
+            }
+            /*
+            else { // . . .
+                    //try (FileInputStream input = new FileInputStream("C:\\Users\\alebr\\Documents\\FoodDelivery\\FoodDelivery\\PropertiesManager\\src\\main\\resources\\config.properties")) {
+                    try ( InputStream input = PropertiesManager.class.getClassLoader().getResourceAsStream("config.properties") ) {
+                        prop.load(input);
+                    }catch (Exception e) { System.out.println("File non trovato"); }
+            }
+            */
+        }
+        return prop;
+    }
+}
